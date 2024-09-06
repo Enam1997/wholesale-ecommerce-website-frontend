e24c55028b0de9ac5280bb2e1c57e962d9d8b1fe
Enam <mdenamahmedchowdhury@gmail.com>
2024-09-06 18:28:51 +0600

    Make changes on footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,50 +1,119 @@
-import React from 'react';
-import './footer.css';
-import { Facebook, Twitter, Instagram, LinkedIn } from '@mui/icons-material';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  IconButton,
+  Divider,
+} from "@mui/material";
+import { Facebook, Twitter, Instagram, LinkedIn } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__sections">
-        <div className="footer__section">
-          <h3>About Us</h3>
-          <p>
-            We are a leading e-commerce platform providing a wide range of products to our customers.
-            Our mission is to deliver quality products at the best prices.
-          </p>
-        </div>
-        <div className="footer__section">
-          <h3>Quick Links</h3>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact Us</a></li>
-          </ul>
-        </div>
-        <div className="footer__section">
-          <h3>Customer Service</h3>
-          <ul>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Shipping & Returns</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms & Conditions</a></li>
-          </ul>
-        </div>
-        <div className="footer__section">
-          <h3>Follow Us</h3>
-          <div className="footer__socials">
-            <a href="#"><Facebook /></a>
-            <a href="#"><Twitter /></a>
-            <a href="#"><Instagram /></a>
-            <a href="#"><LinkedIn /></a>
-          </div>
-        </div>
-      </div>
-      <div className="footer__bottom">
-        <p>&copy; {new Date().getFullYear()} Your Company Name. All Rights Reserved.</p>
-      </div>
-    </footer>
+    <Box sx={{ backgroundColor: "#333", color: "#fff", padding: "2rem 1rem" }}>
+      <Container>
+        <Grid container spacing={4} mb={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6" gutterBottom sx={{ color: "#e74c3c" }}>
+              About Us
+            </Typography>
+            <Typography variant="body2" paragraph>
+              We are a leading e-commerce platform providing a wide range of
+              products to our customers. Our mission is to deliver quality
+              products at the best prices.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6" gutterBottom sx={{ color: "#e74c3c" }}>
+              Quick Links
+            </Typography>
+            <Typography variant="body2">
+              <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+                Home
+              </Link>
+              <br />
+              <Link
+                to="/shop"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Shop
+              </Link>
+              <br />
+              <Link
+                to="/blog"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Blog
+              </Link>
+              <br />
+              <Link
+                to="/contact"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Contact Us
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6" gutterBottom sx={{ color: "#e74c3c" }}>
+              Customer Service
+            </Typography>
+            <Typography variant="body2">
+              <Link to="/faq" style={{ textDecoration: "none", color: "#fff" }}>
+                FAQ
+              </Link>
+              <br />
+              <Link
+                to="/shipping-returns"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Shipping & Returns
+              </Link>
+              <br />
+              <Link
+                to="/privacy-policy"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Privacy Policy
+              </Link>
+              <br />
+              <Link
+                to="/terms-conditions"
+                style={{ textDecoration: "none", color: "#fff" }}
+              >
+                Terms & Conditions
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Typography variant="h6" gutterBottom sx={{ color: "#e74c3c" }}>
+              Follow Us
+            </Typography>
+            <Box>
+              <IconButton component={Link} to="#" sx={{ color: "#fff", mr: 1 }}>
+                <Facebook />
+              </IconButton>
+              <IconButton component={Link} to="#" sx={{ color: "#fff", mr: 1 }}>
+                <Twitter />
+              </IconButton>
+              <IconButton component={Link} to="#" sx={{ color: "#fff", mr: 1 }}>
+                <Instagram />
+              </IconButton>
+              <IconButton component={Link} to="#" sx={{ color: "#fff", mr: 1 }}>
+                <LinkedIn />
+              </IconButton>
+            </Box>
+          </Grid>
+        </Grid>
+        <Divider sx={{ borderColor: "#444", mb: 2 }} />
+        <Typography variant="body2" align="center" sx={{ color: "#aaa" }}>
+          &copy; {new Date().getFullYear()} Your Company Name. All Rights
+          Reserved.
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 

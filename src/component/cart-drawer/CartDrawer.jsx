ad508dbca2f5ebe2656767718ed4e2ee78e96fbc
Enam <mdenamahmedchowdhury@gmail.com>
2024-10-26 18:11:59 +0600

    Make changes

--- src/component/cart-drawer/CartDrawer.jsx
+++ src/component/cart-drawer/CartDrawer.jsx
@@ -17,7 +17,8 @@ import { useNavigate } from "react-router-dom"; // to navigate between routes
 import { useCart } from "../../context/CartContext";
 import { productImageLink } from "../../api";
 import calculateDiscountPrice from "../../utils/calculateProductDiscountPrice";
-import { totalProductPrice } from "../../utils/orderPrice";
+import { calculateTotalPrice } from "../../utils/orderPrice";
+
 
 const CartDrawer = ({ open, onClose, demoCartItems }) => {
   const [cart, setCart] = useState(demoCartItems);
@@ -170,7 +171,7 @@ const CartDrawer = ({ open, onClose, demoCartItems }) => {
           <Typography variant="h6">Total</Typography>
           <Typography variant="h6">
             {" "}
-            {`AED ${totalProductPrice(cartItems)}`}
+            {`AED ${calculateTotalPrice(cartItems)}`}
           </Typography>
         </Box>
         <Button
--- src/utils/orderPrice.js
+++ src/utils/orderPrice.js
@@ -1,6 +1,18 @@
-function totalProductPrice() {
-  const total = 400;
-  return total;
-}
+import calculateDiscountPrice from "./calculateProductDiscountPrice";
 
-export { totalProductPrice };
+function calculateTotalPrice(cartItems) {
+    let totalPrice = 0;
+  
+    cartItems.forEach(item => {
+      const { price, discount, quantity } = item;
+      const effectivePrice = discount
+        ? calculateDiscountPrice(price, discount)
+        : price;
+  
+      totalPrice += effectivePrice * quantity;
+    });
+  
+    return totalPrice.toFixed(2); // Round to 2 decimal places
+  }
+
+export { calculateTotalPrice };
